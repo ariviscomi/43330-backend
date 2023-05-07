720666b447e9e07380fa6d3ea61bdc4f77e93459
ariviscomi <arielviscomi.mail@gmail.com>
2023-05-07 16:20:12 -0300

    ###

--- Desafio02/app.js
+++ Desafio02/app.js
@@ -1,76 +1,102 @@
+const { Console } = require('console');
 const fs = require('fs');
+const format = 'utf-8';
 
-const writeProduct = async () => {
+/* const writeProduct = async () => {
 
-	try{
+	try {
 
 	}
 
-	catch(error){
+	catch (error) {
 
 	}
 }
 
 const readProduct = async () => {
 
-	try{
+	try {
 
 	}
 
-	catch(error){
+	catch (error) {
 
 	}
 }
 
 const findProduct = async () => {
 
-	try{
+	try {
 	}
 
-	catch(error){
+	catch (error) {
 	}
-}
+} */
 class ProductManager {
 
-	constructor() {
-		this.products = [];
+	constructor(path) {
+		this.path = path;
+		this.products=[];
+
+		fs.promises.writeFile(this.path,JSON.stringify(this.products));
+		console.log('Nuevo archivo creado');
 	}
-	
-	addProduct(path,title, description, price, thumbnail, code, stock) {
-		console.log('\nAgregando un nuevo producto:');
 
-		const product = {
-			path : path,
-			id : this.products.length + 1,
-			title : title,
-			description : description,
-			price : price,
-			thumbnail : thumbnail,
-			code : code,
-			stock : stock
-		};
+	addProduct = async (title, description, price, thumbnail, code, stock) => {
 
-		if (typeof title === undefined || typeof description === undefined || typeof price === undefined || typeof thumbnail === undefined || typeof code === undefined || typeof stock === undefined) {
-			console.warn('Todos los parametros son obligatorios!!');
-		} else {
-			const prodFound = this.products.some((_item) => _item.code == code);
-			if (prodFound) {
-				console.log('Ya existe un producto con ese Codigo');
+		try {
+
+			this.products = JSON.parse(await fs.promises.readFile(this.path,format));
+			
+			console.log('\nAgregando un nuevo producto:');
+
+			const product = {
+				id: this.products.length + 1,
+				title: title,
+				description: description,
+				price: price,
+				thumbnail: thumbnail,
+				code: code,
+				stock: stock
+			};
+
+			if (typeof title === undefined || typeof description === undefined || typeof price === undefined || typeof thumbnail === undefined || typeof code === undefined || typeof stock === undefined) {
+				console.warn('Todos los parametros son obligatorios!!');
 			} else {
-				this.products.push(product);
-				console.log('Nuevo producto agregado!');
+				const prodFound = this.products.some((_item) => _item.code == code);
+				if (prodFound) {
+					console.warn('Ya existe un producto con ese Codigo');
+				} else {
+					await fs.promises.appendFile(this.path, JSON.stringify(product));
+					console.log('Nuevo producto agregado!');
+				}
 			}
 		}
+
+		catch (err) { console.error('Algo Malio Sal :s') }
 	}
 
-	getProducts() {
-		console.log('\nMostrando productos de la lista:')
-		if (this.products.length) {
-			console.log(this.products);
-			return this.products;
-		} else {
-			console.warn('No hay productos en esta lista');
+	unlinkFile = async () => {
+		try {
+			await fs.promises.unlink(this.path);
+			console.log('Se desvinculo el archivo');
 		}
+
+		catch (err) { console.warn('No se pudo desvincular el archivo') }
+	}
+
+	getProducts = async () => {
+		console.log('\nMostrando productos de la lista:');
+
+		try {
+
+			let products = await fs.promises.readFile(this.path,format)
+
+			console.log(products);
+			return products;
+		}
+
+		catch (err) { console.warn('No se pudo traer el listado') }
 	}
 
 	getProductById(id) {
@@ -79,10 +105,18 @@ class ProductManager {
 		if (product == undefined) {
 			console.error('Not Found');
 		} else {
-			console.log('El producto que busca es este: ',product);
+			console.log('El producto que busca es este: ', product);
 			return product;
 		}
 	}
 }
 
-const prodManager = new ProductManager;
+//TODO: ↓↓ Prueba de codigo ↓↓
+
+const prodManager = new ProductManager('./files/products.txt');
+
+prodManager.addProduct("mate montaña", "Mate con dibujo de montañas", 1200, "imagen", "aa022", 10);
+prodManager.addProduct("mate termico", "Mate de acero termico", 2000, "imagen", "aa022", 30);
+
+// prodManager.getProducts();
+// prodManager.unlinkFile();
