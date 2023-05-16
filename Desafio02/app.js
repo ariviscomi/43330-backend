a27963b8441698928070297d53ed87daa7576880
ariviscomi <arielviscomi.mail@gmail.com>
2023-05-16 12:47:43 -0300

    Desafio 2

--- Desafio02/app.js
+++ Desafio02/app.js
@@ -1,44 +1,70 @@
 const fs = require('fs');
 
 class ProductManager {
-	constructor(filePath) {
-		this.filePath = filePath;
-		this.products = [];
-	}
+  constructor(filePath) {
+    this.filePath = filePath;
+    this.products = [];
+  }
 
-	loadProducts() {
-		return fs.promises.readFile(this.filePath, 'utf-8')
-			.then(data => {
-				this.products = data.split('\n');
-				return this.products;
-			})
-	}
+  loadProducts() {
+	return fs.promises.readFile(this.filePath, 'utf-8')
+	.then(data => {
+	  this.products = JSON.parse(data);
+	  return this.products;
+	});
+  }
 
-	saveProducts() {
-		const data = this.products.join('\n');
-		return fs.promises.writeFile(this.filePath, data);
-	}
+  saveProducts() {
+    const data = JSON.stringify(this.products);
+    return fs.promises.writeFile(this.filePath, data);
+  }
 
-	addProduct(product) {
-		this.products.push(product);
-		return this.saveProducts();
-	}
+  addProduct(product) {
+    product.id = this.products.length + 1;
+    this.products.push(product);
+    return this.saveProducts();
+  }
 
-	updateProduct(index, product) {
-		this.products[index] = product;
-		return this.saveProducts();
-	}
+  updateProduct(id, updatedProduct) {
+    const index = this.products.findIndex(product => product.id === id);
+    if (index !== -1) {
+      this.products[index] = { ...updatedProduct, id };
+      return this.saveProducts();
+    } else {
+      return Promise.reject(new Error('Product not found'));
+    }
+  }
 
-	removeProduct(index) {
-		this.products.splice(index, 1);
-		return this.saveProducts();
-	}
+  getAllProducts() {
+    return this.loadProducts();
+  }
 
-	listProducts() {
-		return this.loadProducts()
-			.then(products => {
-				console.log(products);
-				return products;
-			})
-	}
+  getProductById(id) {
+    const product = this.products.find(product => product.id === id);
+    if (product) {
+      return Promise.resolve(product);
+    } else {
+      return Promise.reject(new Error('Product not found'));
+    }
+  }
 }
+
+const productManager = new ProductManager('files/products.txt');
+
+productManager.addProduct({
+  title: 'Producto 1',
+  description: 'Descripción del producto 1',
+  price: 9.99,
+  thumbnail: 'ruta/imagen1.jpg'
+})
+
+productManager.addProduct({
+  title: 'Producto 2',
+  description: 'Descripción del producto 2',
+  price: 9.99,
+  thumbnail: 'ruta/imagen2.jpg'
+})
+
+  .then(() => {return productManager.getAllProducts();})
+  .then(products => {console.log(products);})
+  .catch(error => {console.error(error);});
