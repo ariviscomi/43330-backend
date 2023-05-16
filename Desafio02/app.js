7e31e789c5f759a89d9dd7a1f162c908ff4bf3d6
ariviscomi <arielviscomi.mail@gmail.com>
2023-05-16 11:38:13 -0300

    Desafio 2

--- Desafio02/app.js
+++ Desafio02/app.js
@@ -1,93 +1,44 @@
 const fs = require('fs');
-const format = 'utf-8';
 
 class ProductManager {
-
-	constructor(path) {
-		this.path = path;
-		fs.promises.writeFile(this.path,'');
+	constructor(filePath) {
+		this.filePath = filePath;
+		this.products = [];
 	}
 
-	addProduct = async (title, description, price, thumbnail, code, stock) => {
-
-		console.log('\nAgregando un nuevo producto:');
-
-		const product = {
-			id: 0,
-			title: title,
-			description: description,
-			price: price,
-			thumbnail: thumbnail,
-			code: code,
-			stock: stock
-		};
-
-		try {
-			console.log('leyendo archivo');
-		
-			let products = await fs.promises.readFile(this.path, format);
-			JSON.parse(products);
-
-			// product.id = products.length + 1;
-
-			await fs.promises.appendFile(this.path,JSON.stringify(product) + ',');
-		}
-
-		catch {
-			console.warn('no se pudo encontrar el archivo');
-			console.log('creando archivo');
-			try {
-				this.products.push(product);
-				await fs.promises.writeFile(this.path, JSON.stringify(this.products));
-			}
-
-			catch {
-				console.error('Algo Malio Sal');
-			}
-		}
+	loadProducts() {
+		return fs.promises.readFile(this.filePath, 'utf-8')
+			.then(data => {
+				this.products = data.split('\n');
+				return this.products;
+			})
 	}
 
-	unlinkFile = async () => {
-		try {
-			await fs.promises.unlink(this.path);
-			console.log('Se desvinculo el archivo');
-		}
-
-		catch (err) { console.warn('No se pudo desvincular el archivo') }
+	saveProducts() {
+		const data = this.products.join('\n');
+		return fs.promises.writeFile(this.filePath, data);
 	}
 
-	getProducts = async () => {
-		console.log('\nMostrando productos de la lista:');
-
-		try {
-
-			let products = await fs.promises.readFile(this.path, format)
-
-			console.log(products);
-			return products;
-		}
-
-		catch (err) { console.warn('No se pudo traer el listado') }
+	addProduct(product) {
+		this.products.push(product);
+		return this.saveProducts();
 	}
 
-	getProductById(id) {
-		console.log('\nBuscando producto:')
-		const product = this.products.find(prod => prod.id === id);
-		if (product == undefined) {
-			console.error('Not Found');
-		} else {
-			console.log('El producto que busca es este: ', product);
-			return product;
-		}
+	updateProduct(index, product) {
+		this.products[index] = product;
+		return this.saveProducts();
+	}
+
+	removeProduct(index) {
+		this.products.splice(index, 1);
+		return this.saveProducts();
+	}
+
+	listProducts() {
+		return this.loadProducts()
+			.then(products => {
+				console.log(products);
+				return products;
+			})
 	}
 }
-
-//TODO: ↓↓ Prueba de codigo ↓↓
-
-const prodManager = new ProductManager('./files/products.txt');
-
-prodManager.addProduct("mate montaña", "Mate con dibujo de montañas", 1200, "imagen", "aa022", 10);
-prodManager.addProduct("mate termico", "Mate de acero termico", 2000, "imagen", "aa022", 30);
-
-// prodManager.getProducts();
-// prodManager.unlinkFile();
